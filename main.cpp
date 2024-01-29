0ea777de87a242121906ac4de550ecfde1306063
jha-kundan <152714394+jha-kundan@users.noreply.github.com>
2024-01-29 12:49:09 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 5;
+    int b = 6;
+    int c = a + b;
+    cout << c << endl;
+    int d = c - a;
+    cout << d;
+    int e = c * a - d;
+    cout << d << endl;
+    float a = 5.2;
+    float b = 2.2;
+    float c = a + b;
+    cout << c;
+    float a = 42.0;
+    float b = 21;
+    float c = a / b;
+    cout << c;
+}
